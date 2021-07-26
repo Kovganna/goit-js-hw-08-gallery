9a83eef28be5e7ac0ba073f606ad8381eb5d9f7a
Ganna <annakovalenko168@gmail.com>
2021-07-26 11:07:07 +0300

    finish

--- task.js
+++ task.js
@@ -63,6 +63,7 @@ else {
 window.addEventListener('keydown', keyEscape);
 }
 
+
 function keyEnterClick(event) {
   if (event.code === "Enter" && event.target.nodeName === "A") {
     refs.createModalImg.classList.add("is-open");
@@ -85,4 +86,35 @@ function keyEscape(event) {
     }
     
 }
+
+window.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowLeft") {
+    onArrowLeft();
+  }
+  if (event.code === "ArrowRight") {
+    onArrowRight();
+  }
+});
+
+// function onArrowLeft() {
+//   let index = +refs.lightboxImage.dataset.index;
+//   if (index === 0) {
+//     step(galleryItems.length - 1);
+//     return;
+//   }
+//   step(index, -1);
+// }
+// function onArrowRight() {
+//   let index = +refs.lightboxImage.dataset.index;
+//   if (index === galleryItems.length - 1) {
+//     step(0);
+//     return;
+//   }
+//   step(index, 1);
+// }
+
+// function step(index, step = 0) {
+//   refs.lightboxImage.dataset.index = `${index + step}`;
+//   refs.lightboxImage.src = galleryItems[index + step].original;
+// }
 
