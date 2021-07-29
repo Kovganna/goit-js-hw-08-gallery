68b586a7c9c05f660a26fa6e447f6d383b6669ef
Ganna <annakovalenko168@gmail.com>
2021-07-29 20:06:04 +0300

    корректировка

--- task.js
+++ task.js
@@ -96,25 +96,4 @@ window.addEventListener("keydown", (event) => {
   }
 });
 
-// function onArrowLeft() {
-//   let index = +refs.lightboxImage.dataset.index;
-//   if (index === 0) {
-//     step(galleryItems.length - 1);
-//     return;
-//   }
-//   step(index, -1);
-// }
-// function onArrowRight() {
-//   let index = +refs.lightboxImage.dataset.index;
-//   if (index === galleryItems.length - 1) {
-//     step(0);
-//     return;
-//   }
-//   step(index, 1);
-// }
-
-// function step(index, step = 0) {
-//   refs.lightboxImage.dataset.index = `${index + step}`;
-//   refs.lightboxImage.src = galleryItems[index + step].original;
-// }
 
